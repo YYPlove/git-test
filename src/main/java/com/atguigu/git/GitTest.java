d320e8dc2de1e79b1d7bab4ab550833d51b81a19
yyp <2218097514@qq.com>
2022-10-16 21:33:07 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author YYP
+ * @create 2022-10-16 21:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
